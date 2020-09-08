0da160799d85eb63fb3c0ff9de7d892aef584c78
Johannes <h-annie@live.co.za>
2020-09-08 13:10:51 +0200

    Added more logs and a call to alphavantage

--- app.js
+++ app.js
@@ -29,9 +29,12 @@ async function getCryptoUsdValue() {
 async function convertDollarToZar(dollarAmount) {
   try {
     const response = await axios.get(
-      "https://api.exchangeratesapi.io/latest?base=USD&symbols=ZAR"
+      "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=ZAR&apikey=GZEAPZVPO6PDQHDS"
     );
-    const currentUsdZar = response.data.rates.ZAR;
+    const currentUsdZar =
+      response.data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+    console.log("Current USD ZAR");
+    console.log(currentUsdZar);
     return dollarAmount * currentUsdZar;
   } catch (error) {
     console.error("convertDollarToZar");
@@ -43,15 +46,23 @@ async function convertDollarToZar(dollarAmount) {
 async function main() {
   const result = await getValrZarValue();
   const lastTradedValrPrice = result.data.lastTradedPrice;
+  console.log("Valr Price");
+  console.log(lastTradedValrPrice);
 
   const cryptoResult = await getCryptoUsdValue();
   const cryptoLastTradedDollarPrice = cryptoResult.data.result.data.a;
+  console.log("Crypto Dollar Price");
+  console.log(cryptoLastTradedDollarPrice);
 
   const cryptoZarAmount = await convertDollarToZar(cryptoLastTradedDollarPrice);
+  console.log("Crypto Zar Price");
+  console.log(cryptoZarAmount);
 
   const currentArb = (1 - cryptoZarAmount / lastTradedValrPrice) * 100;
+  console.log(`=====================`);
   console.log(`Current Arb`);
   console.log(currentArb + "%");
+  console.log(`=====================`);
 }
 
 main().then(console.log(""));
